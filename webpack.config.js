b57cdf518f9da48c987f03802e986f7f95faf160
Микола Шуляк <mykolashuliak@MacBook-Pro-Mikola.local>
2024-11-27 20:06:09 +0200

    creat project

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: {
+      main: './src/scripts/index.js', // главный JS файл
+      styles: './src/styles/main.sass', // главный SASS файл
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'scripts/[name].[contenthash].js',
+      assetModuleFilename: 'assets/images/[hash][ext][query]', // для изображений
+      clean: true,
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader', // Для DEV используем style-loader
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    optimization: {
+      minimize: isProd,
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin(),
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: isProd ? 'styles/[name].[contenthash].css' : 'styles/[name].css', // Без хеша для DEV
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/pages/about.html',
+        filename: 'pages/about.html',
+        inject: 'body',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/pages/menu.html',
+        filename: 'pages/menu.html',
+        inject: 'body',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/pages/order.html',
+        filename: 'pages/order.html',
+        inject: 'body',
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: './src/assets', to: 'assets' },
+        ],
+      }),
+    ],
+  };
+};
