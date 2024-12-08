08dea87943feae291f74cf36d2069635c8fd23ed
Микола Шуляк <mykolashuliak@MacBook-Pro-Mikola.local>
2024-12-08 17:10:18 +0200

    update some style img and js? local on file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,53 @@
 console.log('test');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const carouselImg = document.querySelector('.carousel-img img');
+    const leftButton = document.querySelector('.carousel-left');
+    const rightButton = document.querySelector('.carousel-right');
+
+    const images = [
+        'assets/images/main-pizza.jpg',
+        'assets/images/main-icon.jpg',
+        'assets/images/icon-brouser.jpg'
+    ];
+    let currentIndex = 0;
+    let interval;
+
+    function changeImage(index) {
+        carouselImg.src = images[index];
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        changeImage(currentIndex);
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        changeImage(currentIndex);
+    }
+
+    function startAutoRotate() {
+        interval = setInterval(nextImage, 2000); 
+    }
+
+    function stopAutoRotate() {
+        clearInterval(interval);
+    }
+    
+    rightButton.addEventListener('click', function () {
+        stopAutoRotate();
+        nextImage();
+        startAutoRotate();
+    });
+
+    leftButton.addEventListener('click', function () {
+        stopAutoRotate();
+        prevImage();
+        startAutoRotate();
+    });
+
+    changeImage(currentIndex);
+    startAutoRotate();
+});
